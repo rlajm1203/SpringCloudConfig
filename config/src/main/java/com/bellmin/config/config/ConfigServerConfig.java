b04b8701553591df7e20a226f95b408277050fcf
rlajm1203 <whdlas011203@naver.com>
2025-03-11 14:43:50 +0900

    refactor: @Configuration 추가

--- config/src/main/java/com/bellmin/config/config/ConfigServerConfig.java
+++ config/src/main/java/com/bellmin/config/config/ConfigServerConfig.java
@@ -1,7 +1,9 @@
 package com.bellmin.config.config;
 
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @EnableConfigServer
+@Configuration
 public class ConfigServerConfig {
 }
