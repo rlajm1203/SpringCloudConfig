abb46c340737eb1f6432e25db2a951a68b6b0524
Bellmin <whdals011203@naver.com>
2025-10-22 09:20:52 +0900

    delete: 필요 없는 파일들 삭제

--- config/src/main/java/com/bellmin/config/config/ConfigServerConfig.java
+++
@@ -1,9 +0,0 @@
-package com.bellmin.config.config;
-
-import org.springframework.cloud.config.server.EnableConfigServer;
-import org.springframework.context.annotation.Configuration;
-
-@EnableConfigServer
-@Configuration
-public class ConfigServerConfig {
-}
